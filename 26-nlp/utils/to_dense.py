4a111d77ed5d7abf72e5b78cce9586bf0495541e
Thais <thais@MacBook-Pro-van-MacBook.local>
2019-06-27 01:29:48 -0300

    Adding class 26-nlp

---
+++ 26-nlp/utils/to_dense.py
@@ -0,0 +1,9 @@
+from sklearn.base import TransformerMixin
+
+class DenseTransformer(TransformerMixin):
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, y=None, **fit_params):
+        return X.todense()
